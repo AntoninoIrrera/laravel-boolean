feaf8f7207e884655dfcc5fa79b7811051e4e299
SaraDonzellini <saradonzie@gmail.com>
2023-03-07 15:29:45 +0100

    starting  with pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script >
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 import axios from 'axios';
 import { store } from './store.js';
 
 export default {
   name: 'App',
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      
+    }
+  },
 }
 </script>
 
 <template>
-
+  <AppHeader />
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
--- src/router.js
+++ src/router.js
@@ -1,11 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import HomePage from './pages/HomePage.vue';
+import LibraryList from './pages/LibraryList.vue';
+import AboutUs from './pages/AboutUs.vue';
+// import SingleProject from './pages/SingleProject.vue';
+
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      
-    }
+      path: '/',
+      name: 'homepage',
+      component: HomePage
+    },
+    {
+      path: '/library',
+      name: 'library',
+      component: LibraryList
+    },
+    {
+      path: '/aboutus',
+      name: 'aboutus',
+      component: AboutUs
+    },
+    // {
+    //   path: '/projects/:slug',
+    //   name: 'singleProject',
+    //   component: SingleProject
+    // },
 
   ]
 });
