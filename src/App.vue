a8994d27a3663e9963a30381743ced9bd8973993
SaraDonzellini <saradonzie@gmail.com>
2023-03-07 15:06:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { store } from './store.js';
+
+export default {
+  name: 'App',
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use 'bootstrap/scss/bootstrap' as *;
+</style>
