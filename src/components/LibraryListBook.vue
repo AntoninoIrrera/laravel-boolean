48f3ed5d33164826b285cb383648b795900f281b
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-09 12:43:39 +0100

    Fixed card style

--- src/components/LibraryListBook.vue
+++ src/components/LibraryListBook.vue
@@ -11,14 +11,19 @@ export default {
         }} ({{ book.price }}&euro;)</div>
         <img class="card-img-top" :src="book.cover_image" :alt="book.title">
         <div class="card-body">
-            <h5 class="card-title">{{ book.title }}</h5>
+            <h5 class="card-title fw-bold">{{ book.title }}</h5>
             <p class="card-text">{{ book.description }}</p>
             <router-link :to="{ name: 'singlebook', params: { id: book.id } }" class="btn btn-success">Read
                 more...</router-link>
 
         </div>
         <div class="card-footer">
-            Editor: {{ book.editor }} - - - <span v-for="genre in book.genres">#{{ genre.name }}</span>
+            <span class="d-block mb-3">
+                Editor: {{ book.editor }}
+            </span>
+            <span v-for="genre in book.genres" class="me-2 p-2" :style="{ 'background-color': genre.color }">
+                #{{ genre.name }}
+            </span>
         </div>
     </div>
 </template>
